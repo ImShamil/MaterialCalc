6a38fc0dbe78f03e786e19287dc7cc0862e0dea6
Imandinov_Shamil <flip0173@gmail.com>
2022-10-05 00:03:47 +0400

    fix

--- ConsoleApp6/Properties/AssemblyInfo.cs
+++ ConsoleApp6/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов для изменения сведений,
 // связанные с этой сборкой.
-[assembly: AssemblyTitle("ConsoleApp6")]
+[assembly: AssemblyTitle("MaterialCalc")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp6")]
+[assembly: AssemblyProduct("MaterialCalc")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

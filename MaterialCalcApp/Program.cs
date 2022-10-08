74b291cec42745d0c4f9d98eea5c54a402cfeb38
Imandinov_Shamil <flip0173@gmail.com>
2022-10-08 21:49:50 +0400

    add sum material list

--- MaterialCalcApp/Program.cs
+++ MaterialCalcApp/Program.cs
@@ -8,37 +8,44 @@ namespace MaterialCalc
 {
     internal class Program
     {
-        public static string TableRow(string cellName, string cellMaterial, string cellParam, string cellQuantiti,string cellTotal)
+        const int stringSize = 30;
+        public static string TableHead(string cellName, string cellMaterial, string cellParam, string cellQuantiti, string cellTotal)
         {
-            string tableRow = string.Format("|{0,20}|{1,20}|{2,20}|{3,20}|{4,20}|", cellName, cellMaterial, cellParam,cellQuantiti,cellTotal);
-            return tableRow;
+            string tableHead = ($"|{cellName,stringSize}|{cellMaterial,stringSize}|{cellParam,stringSize}|{cellQuantiti,stringSize}|{cellTotal,stringSize}|");
+            return tableHead;
         }
+        public static void TableContent(string cellName, string cellMaterial, string cellParam, string cellQuantiti, string cellTotal)
+        {
+            string tableContent = ($"|{cellName,-stringSize}|{cellMaterial,stringSize}|{cellParam,stringSize}|{cellQuantiti,stringSize}|{cellTotal,stringSize}|");
+            Console.WriteLine(tableContent);
+        }
+
 
         public struct Assemble
         {
             string name;
             int count;
             int totalCount;
-            List <Assemble> assemblesList;
-            List <Part> partList;
-           
-            public Assemble (string name, int count, int assembleCount=1 )
+            List<Assemble> assemblesList;
+            List<Part> partList;
+
+            public Assemble(string name, int count, int assembleCount = 1)
             {
-                if (name=="")
+                if (name == "")
                 {
                     name = "Сборка";
                 }
 
-                if (count==0)
+                if (count == 0)
                 {
-                    count =1;
+                    count = 1;
                 }
 
                 this.name = name;
                 this.count = count;
-                this.totalCount=count *assembleCount;
-                assemblesList=new List<Assemble>(); //здесь лежат подсборки
-                partList=new List<Part>();//здесь лежат детали
+                this.totalCount = count * assembleCount;
+                assemblesList = new List<Assemble>(); //здесь лежат подсборки
+                partList = new List<Part>();//здесь лежат детали
             }
 
             public string GetName()
@@ -56,7 +63,7 @@ namespace MaterialCalc
                 return this.totalCount;
             }
 
-            public void AddAssemble (Assemble assemble)
+            public void AddAssemble(Assemble assemble)
             {
                 assemblesList.Add(assemble);
             }
@@ -66,7 +73,7 @@ namespace MaterialCalc
                 return assemblesList[index];
             }
 
-             public void AddPart (Part part)
+            public void AddPart(Part part)
             {
                 partList.Add(part);
             }
@@ -86,10 +93,10 @@ namespace MaterialCalc
             {
                 return partList.Count;
             }
-           
+
         }
 
-        public struct  Part
+        public struct Part
         {
             string name;
             string material_name;
@@ -97,11 +104,11 @@ namespace MaterialCalc
             int quantity;
             int totalQuantity;
             double total;
+            double totalForOne;
 
- 
-            public Part(string name, string material,string param,string quantity,int assembleQuantity)
+            public Part(string name, string material, string param, string quantity, int assembleQuantity)
             {
-                if (name=="")
+                if (name == "")
                 {
                     name = "Деталь";
                 }
@@ -116,15 +123,16 @@ namespace MaterialCalc
                 }
                 if (quantity == "")
                 {
-                    quantity="1";
+                    quantity = "1";
                 }
 
                 this.name = name;
                 this.material_name = material.ToLower();
-                this.param =Convert.ToDouble( param);
-                this.quantity=Convert.ToInt32( quantity);
-                this.totalQuantity=this.quantity*assembleQuantity;
-                this.total=this.param*Convert.ToDouble(totalQuantity);
+                this.param = Convert.ToDouble(param);
+                this.quantity = Convert.ToInt32(quantity);
+                this.totalQuantity = this.quantity * assembleQuantity;
+                this.total = this.param * Convert.ToDouble(totalQuantity);
+                this.totalForOne = this.param * this.quantity;
             }
 
             public string GetName()
@@ -147,80 +155,89 @@ namespace MaterialCalc
             }
             public int GetTotalQuantiti()
             {
-                 return this.totalQuantity;
+                return this.totalQuantity;
             }
 
             public double GetTotal()
             {
                 return this.total;
             }
+
+            public double GetTotalForOne()
+            {
+                return this.totalForOne;
+            }
         };
 
-     
-        public static void showAssembles(Assemble assemble,int repeat=0,string step="")
-        {   
-            string stepAssambleName=step+assemble.GetName();
-            string quantityStringAssemble=$"{Convert.ToString(assemble.GetTotalCount())}"+$"({assemble.GetCount()})";
-            Console.WriteLine($"|{stepAssambleName,-20}|{"",-20}|{"",-20}|{quantityStringAssemble,20}|{"",-20}|" );
-            repeat++;
-            string Newstep=new String(' ', repeat);
-           foreach( Assemble assembleItem in assemble.GetAssembleList())
-            {  
-                showAssembles(assembleItem,repeat,Newstep);
-            }
-           foreach (Part partItem in assemble.GetPartList())
-           {    
-                string stepPartName=Newstep+partItem.GetName();
-                string quantityString = $"{Convert.ToString(partItem.GetTotalQuantiti())}"+$"({Convert.ToString(partItem.GetQuantiti())})";
-                Console.WriteLine($"|{stepPartName,-20}|{partItem.GetMaterial(),-20}|{Convert.ToString(partItem.GetParam()),-20}|{quantityString,20}|{Convert.ToString(partItem.GetTotal()),20}|");
-                 
-           }
-        }
 
-         public static void ShowAllTable(Assemble assemble)
+        public static void showAssembles(Assemble assemble, int repeat = 0, string step = "")
         {
-                Console.Clear();
-                string tableRow=TableRow("Название", "Материал", "Параметр", "Количество","Всего");
-                Console.WriteLine(tableRow);
-                    for (int i=0; i < tableRow.Length; i++)
-                    {
-                        Console.Write('_');
-                    }
-                    Console.Write('\n');
-                 showAssembles(assemble);
-                    for (int i=0; i < tableRow.Length; i++)
-                    {
-                        Console.Write('_');
-                    }
-                    Console.Write('\n');
+            string stepAssambleName = step + assemble.GetName();
+            string quantityStringAssemble = $"{Convert.ToString(assemble.GetTotalCount())}" + $"({assemble.GetCount()})";
+            TableContent(stepAssambleName, "", "", quantityStringAssemble, "");
+            repeat++;
+            string Newstep = new String(' ', repeat);
+            foreach (Assemble assembleItem in assemble.GetAssembleList())
+            {
+                showAssembles(assembleItem, repeat, Newstep);
+            }
+            foreach (Part partItem in assemble.GetPartList())
+            {
+                string stepPartName = Newstep + partItem.GetName();
+                string quantityString = $"{Convert.ToString(partItem.GetTotalQuantiti())}" + $"({Convert.ToString(partItem.GetQuantiti())})";
+                string totalForOneString = $"{Convert.ToString(partItem.GetTotal())}" + $"({Convert.ToString(partItem.GetTotalForOne())})";
+                TableContent(stepPartName, partItem.GetMaterial(), Convert.ToString(partItem.GetParam()), quantityString, totalForOneString);
+            }
+        }
+
+        public static void ShowAllTable(Assemble assemble, Dictionary<string, double> finalList)
+        {
+            Console.Clear();
+            string tableHead = TableHead("Название", "Материал", "Параметр", "Количество", "Всего");
+            Console.WriteLine(tableHead);
+            for (int i = 0; i < tableHead.Length; i++)
+            {
+                Console.Write('_');
+            }
+            Console.Write('\n');
+            showAssembles(assemble);
+            for (int i = 0; i < tableHead.Length; i++)
+            {
+                Console.Write('_');
+            }
+            Console.Write('\n');
+            ShowFinalList(finalList);
+            Console.Write('\n');
         }
 
 
-        public static void SubAssembleFill(Assemble assemble, Assemble assembleItem)
-        {   ShowAllTable(assemble);
-            Console.WriteLine("Находимся в узле {0}",assembleItem.GetName());
+        public static void AssembleFill(Assemble assemble, Assemble assembleItem, Dictionary<string, double> finalList)
+        { ShowAllTable(assemble,finalList);
+          
+            Console.WriteLine("Находимся в узле {0}", assembleItem.GetName());
             Console.Write("Кол-во подсборок в узле:");
-            int assembleCount=Convert.ToInt32(Console.ReadLine());
+            int assembleCount = Convert.ToInt32(Console.ReadLine());
 
-            for (int i=1; i<=assembleCount;i++)
+            for (int i = 1; i <= assembleCount; i++)
             {
                 Console.Write($"Название подсборки №{i}:");
-                string assembleName=Console.ReadLine();
-            
+                string assembleName = Console.ReadLine();
+
                 Console.Write("Кол-во:");
-                int totalCount=Convert.ToInt32(Console.ReadLine());
-                
-                Assemble subAssemble=new Assemble(assembleName,totalCount,assembleItem.GetTotalCount());
+                int totalCount = Convert.ToInt32(Console.ReadLine());
+
+                Assemble subAssemble = new Assemble(assembleName, totalCount, assembleItem.GetTotalCount());
 
                 assembleItem.AddAssemble(subAssemble);
-                ShowAllTable(assemble);
+               
+                ShowAllTable(assemble, finalList);
             }
             Console.Write("Кол-во деталей в узле:");
-            int partCount=Convert.ToInt32(Console.ReadLine());
+            int partCount = Convert.ToInt32(Console.ReadLine());
 
-            for(int i=1; i <= partCount; i++)
+            for (int i = 1; i <= partCount; i++)
             {
-                 Console.Write($"Введите название детали №{i}:");
+                Console.Write($"Введите название детали №{i}:");
                 string name = Console.ReadLine();
                 Console.Write("Введите название материала:");
                 string material = Console.ReadLine();
@@ -228,26 +245,49 @@ namespace MaterialCalc
                 string param = Console.ReadLine();
                 Console.Write("Введите кол-во:");
                 string quantity = Console.ReadLine();
-                
+
                 if (param.Contains('.'))
                 {
-                   param = param.Replace('.', ',');
+                    param = param.Replace('.', ',');
                 }
-                Part part = new Part(name, material,param,quantity,Convert.ToInt32(assembleItem.GetTotalCount()));
+                Part part = new Part(name, material, param, quantity, Convert.ToInt32(assembleItem.GetTotalCount()));
                 assembleItem.AddPart(part);
-                ShowAllTable(assemble);
+
+                if (finalList.Count == 0)
+                {
+                    finalList.Add(part.GetMaterial(), part.GetTotal());
+                    
+                }
+                else
+                {
+                    if (finalList.ContainsKey(part.GetMaterial()))
+                    {
+                        double newValue = finalList[part.GetMaterial()] + part.GetTotal();
+                        finalList[part.GetMaterial()] = newValue;
+                    }
+                    else
+                    {
+                        finalList.Add(part.GetMaterial(), part.GetTotal());
+                    }
+                }
+               
+                ShowAllTable(assemble, finalList);
             }
 
-            for(int i=1; i<=assembleCount;i++)
+            for (int i = 1; i <= assembleCount; i++)
             {
-                SubAssembleFill(assemble,assembleItem.GetAssembleItem(i-1));
+                AssembleFill(assemble, assembleItem.GetAssembleItem(i - 1), finalList);
             }
-            return ;
+            return;
         }
-
-       
-
-        static void Main(string[] args)
+        public static void ShowFinalList(Dictionary<string, double> finalList)
+        {
+            foreach(var item in finalList)
+            {
+                Console.WriteLine($"{item.Key}............{item.Value}");
+            }
+        }
+    static void Main(string[] args)
         {   
             //List <Part> partList=new List<Part>();
             Console.Write("Название изделия:");
@@ -257,12 +297,9 @@ namespace MaterialCalc
             int totalCount=Convert.ToInt32(Console.ReadLine());
            
             Assemble assemble=new Assemble(assembleName,totalCount);
-            
-           SubAssembleFill(assemble,assemble);
+            var finalList = new Dictionary<string, double>();
 
-            
-            
-            
+            AssembleFill(assemble,assemble, finalList);
             
         }
     }
